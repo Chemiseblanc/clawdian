4bbec4167927baf47bfbbd3a10fbe30854895b82
Matthew Gibson <matt@mgibson.ca>
2026-08-06 20:03:57 -0400

    chore: Add nix devshell support and direnv integration

---
+++ flake.nix
@@ -0,0 +1,32 @@
+{
+  description = "Development environment for Claudian";
+
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+
+  outputs =
+    { nixpkgs, ... }:
+    let
+      systems = [
+        "aarch64-darwin"
+        "aarch64-linux"
+        "x86_64-linux"
+      ];
+      forAllSystems = nixpkgs.lib.genAttrs systems;
+    in
+    {
+      devShells = forAllSystems (
+        system:
+        let
+          pkgs = nixpkgs.legacyPackages.${system};
+        in
+        {
+          default = pkgs.mkShell {
+            packages = with pkgs; [
+              nodejs_24
+              bun
+            ];
+          };
+        }
+      );
+    };
+}
